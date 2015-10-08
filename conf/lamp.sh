33f6cbbb00e943a331b70c72464fbb297d5aab42
Rob Eden <rob.eden@gmail.com>
2015-10-07 20:14:46 -0400

    Cleans up directories and lamp.sh

--- conf/lamp.sh
+++ conf/lamp.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-# cp /etc/resolv.conf /var/spool/postfix/etc/
-
-# if [ ! -z "$MANDRILL_PWD" ]
-# then
-#   echo "Using Mandrill Password. <= Written to /etc/postfix/sasl_passwd"
-
-#   echo "[smtp.mandrillapp.com]:587 $MANDRILL_PWD" > /etc/postfix/sasl_passwd
-#   chown -R postfix:postfix /etc/postfix
-#   chmod 600 /etc/postfix/sasl_passwd
-#   postmap /etc/postfix/sasl_passwd
-# fi
-
-if [ ! -z "$RSYSLOG" ]
-then
-  echo "Adding RSYSLOGD configuration"
-  
-  echo "$RSYSLOG" >> /etc/rsyslog.conf
-fi
-
-/bin/supervisord
-# # TODO: supervisor is not instaling!!!
-# /bin/bash -c "exec /sbin/httpd -D FOREGROUND"
+# Start supervisord in foreground with given configuration
+/bin/supervisord \
+  --nodaemon \
+  --configuration=/etc/supervisord.conf
