6c15b689d8f0c11d7b8f88b99555c7a32be2cd37
Caleb Champlin <caleb.champlin@ocio.usda.gov>
2017-02-08 14:28:11 -0500

    Handle auto updating based on a git repo and git branch

--- conf/run-httpd.sh
+++ conf/run-httpd.sh
@@ -33,4 +33,27 @@ echo "export DOCROOT='$DOCROOT'" > /etc/profile.d/docroot.sh
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
 
+# Perform git pull
+if [ -d "/var/application/www" ]; then
+  if [ -v GIT_BRANCH ]; then
+    git --git-dir=/var/application git checkout $GIT_BRANCH
+    git --git-dir=/var/application git pull origin $GIT_BRANCH
+  fi
+else
+  if [ -v GIT_URL ]; then
+    git clone $GIT_URL /var/application
+    if [ -d "/var/application/www" ]; then
+      if [ -v GIT_BRANCH ]; then
+        git --git-dir=/var/application git checkout $GIT_BRANCH
+        git --git-dir=/var/application git pull origin $GIT_BRANCH
+      fi
+      if [ -d "/var/www/public" ]; then
+        mv /var/www/public /var/www/public_orig
+      fi
+      ln -s /var/application/www /var/www/public
+    fi
+  fi
+fi
+
+
 exec /usr/sbin/apachectl -DFOREGROUND
